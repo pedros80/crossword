b1a95db6270d7cdd4d1fb74395c927a2b5a79563
Pete Somerville <peter.somerville@pitmart.com>
2013-11-14 11:31:39 +0000

    license. header

--- crossword.py
+++ crossword.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+""" 
+crossword.py 
+
+- Check if a word is a word.
+- Get words, sub-words and anagrams from a word or collection of letters.
+- Get possible solutions if your potential solution has words missing. (e.g he__o)
+
+Copyright (C) 2010-2013 Peter Somerville
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 __author__ = "Peter Somerville"
 __email__ = "peterwsomerville@gmail.com"
@@ -176,7 +198,7 @@ class Application(TK.Frame):
         self.resultsFrame.pack()
               
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) == 1:
         app = Application()
     else:
         app = Application(sys.argv[1])
